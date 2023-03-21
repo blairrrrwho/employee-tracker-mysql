e4ff70a572e00e19ad53cfbc0603883d41106ff5
Blair Millet <120157336+blairrrrwho@users.noreply.github.com>
2023-03-21 02:20:32 -0500

    added view all dept, roles, and employees queries

--- db/query.sql
+++ db/query.sql
@@ -1,16 +1,26 @@
 -- view all departments 
 -- SELECT * FROM department
-SELECT id AS ID, dep_name AS 'Department' FROM department
+SELECT id AS 'Department ID', dep_name AS 'Department' 
+FROM department
 
 -- view all roles
 SELECT * FROM roles
 
+
 -- view all employees
-
-
--- SELECT movies.movie_name AS movie, reviews.review
--- FROM reviews
--- LEFT JOIN movies
--- ON reviews.movie_id = movies.id
--- ORDER BY movies.movie_name;
-
+-- SELECT * FROM employees
+SELECT 
+  employees.id AS 'Employee ID', 
+  employees.first_name AS 'First Name', 
+  employees.last_name AS 'Last Name', 
+  roles.title AS 'Job Title', 
+  department.dep_name AS 'Department', 
+  roles.salary AS 'Salary',
+  CONCAT(manager.first_name, ' ', manager.last_name) AS 'Manager'
+FROM 
+  employees
+  JOIN roles ON employees.role_id = roles.id
+  JOIN department ON roles.department_id = department.id
+  LEFT JOIN employees AS manager ON employees.manager_id = manager.id
+ORDER BY 
+  employees.last_name ASC;
